f7fa252b5c482951525d3218a47de89da5cfae9a
Nataliia Pavlyk <natali.pavlyk15@gmail.com>
2022-10-03 00:11:05 +0300

    Start project

---
+++ src/Animals.java
@@ -0,0 +1,35 @@
+public class Animals {
+//        public static String name = "видра";
+//        private static String color;
+//
+//        public String getColor(){
+//            return color;
+//        }
+//        public void setColor(String color) {
+//                this.color = color;
+//                color = "синенькеееееее";
+//        }
+//
+//
+//        public static void main(String[] args) {
+//        System.out.println(color);
+//    }
+
+
+
+
+
+
+}
+
+
+//public class Animals {
+//    static String str = "Hello Java!";
+//    static {
+//        System.out.println(str);
+//    }
+//    public static void main(String[] args) {
+//        System.out.println("Hello from the method 'main'");
+//    }
+//}
+
