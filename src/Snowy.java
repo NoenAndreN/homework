a844752df81eb3f51b919e375be1484c5e25ab70
Nataliia Pavlyk <natali.pavlyk15@gmail.com>
2022-10-04 19:18:28 +0300

    Added name and color to Snowy.java

--- src/Snowy.java
+++ src/Snowy.java
@@ -3,4 +3,12 @@ package src;
 public class Snowy {
     public String name;
     private String color;
+
+    public Snowy (String color) {
+        color = color;
+    }
+    public Snowy() {
+        name = "Сніжок";
+        color = "білий";
+    }
 }
