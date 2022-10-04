8ebc910c74bd935889052be29a0879cafc4d28b0
Nataliia Pavlyk <natali.pavlyk15@gmail.com>
2022-10-04 19:06:26 +0300

    Add file Lutra.java

--- src/Animals.java
+++ src/Animals.java
@@ -1,35 +1,8 @@
+package src;
+
 public class Animals {
-//        public static String name = "видра";
-//        private static String color;
-//
-//        public String getColor(){
-//            return color;
-//        }
-//        public void setColor(String color) {
-//                this.color = color;
-//                color = "синенькеееееее";
-//        }
-//
-//
-//        public static void main(String[] args) {
-//        System.out.println(color);
-//    }
+    public static void main (String[] args) {
 
-
-
-
-
-
+    }
 }
-
-
-//public class Animals {
-//    static String str = "Hello Java!";
-//    static {
-//        System.out.println(str);
-//    }
-//    public static void main(String[] args) {
-//        System.out.println("Hello from the method 'main'");
-//    }
-//}
 
