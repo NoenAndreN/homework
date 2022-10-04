eda2d0fd888d9123502ffcc3968f0b06ac855161
Nataliia Pavlyk <natali.pavlyk15@gmail.com>
2022-10-04 19:22:03 +0300

    Added name and color to Barsyc.java

--- src/Barsyc.java
+++ src/Barsyc.java
@@ -3,4 +3,14 @@ package src;
 public class Barsyc {
     public String name;
     private String color;
+
+
+    public Barsyc (String color) {
+        color = color;
+    }
+
+    public Barsyc() {
+        name = "Барсик";
+        color = "руденький";
+    }
 }
